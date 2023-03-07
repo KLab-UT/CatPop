4db73c63148e635a3e6ed2337b6477acf492a89d
bayleaflet <bayleejchristensen@gmail.com>
2023-03-06 18:25:35 -0700

    Streamlined code, added main.py where we can combine functions

---
+++ main.py
@@ -0,0 +1,20 @@
+# The mainstage where we call our numerous classes and function
+
+# Function that returns every combination of our list, separated into two lists
+# inside one list. Example of a single list: [[A,B,C],[D,E,F]], and in reality,
+# there are hundreds of variations of above, such as [[A,B,E,],[D,C,F]] and so
+# on.
+import get_all_combinations.py
+
+# Function that removes mirror images/duplicates. This would be like removing
+# [[D,E,F], [A,B,C]] from the get_all_combinations, as [[A,B,C],[D,E,F]] and
+# [[D,E,F], [A,B,C]] are mirror 'images'
+import compare.py
+
+
+
+
+
+
+
+
