c11843bfbe5e29f6593bcedf4a41a697d1472ae9
Dantebob <dantecelani@gmail.com>
2023-03-08 17:48:32 -0700

    added remove duplicate to main.py

--- main.py
+++ main.py
@@ -4,20 +4,26 @@
 # inside one list. Example of a single list: [[A,B,C],[D,E,F]], and in reality,
 # there are hundreds of variations of above, such as [[A,B,E,],[D,C,F]] and so
 # on.
-import get_all_combinations
+import get_all_combinations as gac
 
 # Function that removes mirror images/duplicates. This would be like removing
 # [[D,E,F], [A,B,C]] from the get_all_combinations, as [[A,B,C],[D,E,F]] and
 # [[D,E,F], [A,B,C]] are mirror 'images'
-import compare
+from compare import Compare
 
 # Current list of populations we are making the comparisons between
 indv = ['MAH','MER','PVM','SUM','STO','CHR','RON','ROC','ELF','NAM','MAR','PAP']
 
-# When called, returns every possible combinations
-print(get_combinations(indv))
-
-
+# When called, returns every possible combination
+duplicates = gac.get_combinations(indv)
+print(len(duplicates))
+for count, i in enumerate(duplicates):
+    pair = Compare(i[0], i[1])
+    for j in duplicates[count+1:]:
+        c2 = Compare(j[0], j[1])
+        d = pair.Is_duplicate(c2)
+        if d:
+            print(str(d))
 #print(compare.same_diff(get_combinations(indv)))
 
 
