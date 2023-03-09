7e06a4d4f09e84b04594f53ca87b48e89d6808df
reagan.mckee <reagan.mckee@utahtech.edu>
2023-03-09 08:36:21 -0700

    edits

--- Comparisons.py
+++ Comparisons.py
@@ -6,7 +6,7 @@
 
 
 
-example = ['A','B','C','D','E','F']
+example = ['A','B','c','D','E','F']
 FA = [ 'a', 'b', 'c', 'd', 'e', 'f']
 SA = [ 'A', 'B' 'C' 'D', 'E', 'F']
 
@@ -32,6 +32,71 @@ compare(example, FA, SA)
 
 
 
+
+class Comparison:
+    def __init__(self, a, b):
+        self.pair = (a, b)
+        
+    def __eq__(self, c2):
+        if self.pair[0] == c2.pair[0] and self.pair[1] == c2.pair[1]:
+            return True
+        if self.pair[0] == c2.pair[1] and self.pair[1] == c2.pair[0]:
+            return True
+        return False
+    
+    def __str__(self):
+        return (str(self.pair[0]), str(self.pair[1]))
+
+
+#return a list of all possible compares in A
+def getCompares(A):
+    compares = []
+    for i in range(len(A)-1):
+        for j in range(i+1, len(A)):
+            compares.append(Comparison(A[i], A[j]))
+            
+    return compares
+
+#return a tuple of the number of possible compares (same, different) in two lists 
+def checkCompares(A, B):
+    A_comps = getCompares(A)
+    B_comps = getCompares(B)
+    same = 0
+    diff = 0
+    for i in A_comps:
+        if i in B_comps:
+            same +=1
+        else:
+            diff +=1
+    
+    return(same, diff)
+
+#return a tuple of the number of possible compares (same, different) in three lists 
+def checkCompares(A, B, C):
+    A_comps = getCompares(A)
+    B_comps = getCompares(B)
+    C_comps = getCompares(C)
+    same = 0
+    diff = 0
+    found = False
+    for i in A_comps:
+        for j in B_comps:
+            if i==j:
+                same +=1
+                found = True
+        for k in C_comps:
+            if i==k:
+                same +=1
+                found = True
+        if not found:
+            diff+=1
+        
+    
+    return(same, diff)
+
+a = ['a','b','c','d','e','f']
+b = ['x','b','c','d','e','f']
+print(checkCompares(example, FA, SA))
 
 
 '''
