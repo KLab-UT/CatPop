8a6e0b601c4ef87647abb1129bb3cb568ebee9a7
can-dice <candicer2018@gmail.com>
2023-03-29 15:24:23 -0600

    functional geneID dictionary

--- geneID_dict.py
+++ geneID_dict.py
@@ -2,11 +2,12 @@ def make_dict_dict(filename):
     f=open(filename, "r")
     first=f.readline()
     popComb=first.strip().split(",")[1:]
+    dict_geneID={}
     for line in f:
         geneID=line.strip().split(",")[:1]
         fsts=line.strip().split(",")[1:]
         dict_popComb={popComb[0]:fsts[0]}
         for i in range(1,len(popComb)):
             dict_popComb[popComb[i]]=fsts[i]
-        dict_geneID={geneID[0]:dict_popComb}
+        dict_geneID[geneID[0]] = dict_popComb
     return dict_geneID
