812b4b8ed21fa03acf2e5998161406869a39b905
UTU_BioInformatics <utu_bioinformatics@ITs-MacBook-Air.local>
2023-03-30 11:05:51 -0600

    added json stuff

--- geneID_dict.py
+++ geneID_dict.py
@@ -1,13 +1,21 @@
+import json
+
 def make_dict_dict(filename):
     f=open(filename, "r")
     first=f.readline()
-    popComb=first.strip().split(",")[1:]
+    popComb=first.strip().split(",")[1:] #make list of population combinations
     dict_geneID={}
     for line in f:
-        geneID=line.strip().split(",")[:1]
-        fsts=line.strip().split(",")[1:]
+        geneID=line.strip().split(",")[:1]  #get geneID which is the first element in the line
+        fsts=line.strip().split(",")[1:]  #get fst values which are the rest of the elements in the line 
         dict_popComb={popComb[0]:fsts[0]}
         for i in range(1,len(popComb)):
             dict_popComb[popComb[i]]=fsts[i]
         dict_geneID[geneID[0]] = dict_popComb
     return dict_geneID
+
+with open ('geneID_dictionary.json', 'w') as gd:
+    json.dumps(make_dict_dict("exons.popgen.csv"))
+
+#with open('geneID_dictionary.json') as json_file:
+#    dict_geneID = json.load(json_file)
