eea976e5e3d6f25a3228265f28d2b6e3ccd57da7
Dantebob <dantecelani@gmail.com>
2023-04-05 20:51:51 -0600

    fixed random_fst_test.py

--- random_fst_test.py
+++ random_fst_test.py
@@ -4,7 +4,7 @@ def generate_random_numbers(amount):
     i = 0
     rand = []
     while i < amount:
-        rand.append(round(random.uniform(0, 1.0), 6))
+        rand.append(round(random.uniform(0, 1.0), 4))
         i += 1
     return rand
 
@@ -16,9 +16,9 @@ def make_population_pairs(length):
         populations.append("POP"+str(i+1))
     for count, i in enumerate(populations):
         for j in populations[count+1:]:
-            population_pairs.append("FST_"+i+"_"+j)
+            population_pairs.append("Fst_"+i+"_"+j)
 
-def make_fst_file(length):
+def make_fst_file(length, number_of_genes):
     #make population_pairs
     populations = []
     population_pairs = []
@@ -27,11 +27,13 @@ def make_fst_file(length):
     for count, i in enumerate(populations):
         for j in populations[count+1:]:
             population_pairs.append("FST_"+i+"_"+j)
-    #make population_pairs and random fst's into a .csv file
-    fsts = generate_random_numbers(len(population_pairs))
     with open("random_fst_test.csv", "w") as rft:
         rft.write("geneID," + str(population_pairs).strip("[]").replace("'", ""))
-        rft.write("\ngene_1," + str(fsts).strip("[]"))
+        for i in range(number_of_genes):
+            i = str(i+1)
+            #make population_pairs and random fst's into a .csv file
+            fsts = generate_random_numbers(len(population_pairs))
+            rft.write("\ngene_"+ i + "," + str(fsts).strip("[]"))
 
 # make_fst_file(length_of_populations_list)
-make_fst_file(8)
+make_fst_file(8, 547)
