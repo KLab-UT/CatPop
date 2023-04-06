be5462f8ced7ab785460b8d68817c4503decf266
Dantebob <dantecelani@gmail.com>
2023-04-05 19:40:56 -0600

    finished random_fst_test.py

--- random_fst_test.py
+++ random_fst_test.py
@@ -1,5 +1,4 @@
 import random
-import get_all_combinations as gac
 
 def generate_random_numbers(amount):
     i = 0
@@ -8,28 +7,31 @@ def generate_random_numbers(amount):
         rand.append(round(random.uniform(0, 1.0), 6))
         i += 1
     return rand
-        
-generate_random_numbers(10)
 
 #make population list
-length = 10
-populations = []
-population_pairs = []
-for i in range(length):
-    populations.append("POP"+str(i+1))
-for count, i in enumerate(populations):
-    for j in populations[count+1:]:
-        population_pairs.append("FST_"+i+"_"+j)
+def make_population_pairs(length):
+    populations = []
+    population_pairs = []
+    for i in range(length):
+        populations.append("POP"+str(i+1))
+    for count, i in enumerate(populations):
+        for j in populations[count+1:]:
+            population_pairs.append("FST_"+i+"_"+j)
 
-def make_fst_file(population_pairs):
+def make_fst_file(length):
+    #make population_pairs
+    populations = []
+    population_pairs = []
+    for i in range(length):
+        populations.append("POP"+str(i+1))
+    for count, i in enumerate(populations):
+        for j in populations[count+1:]:
+            population_pairs.append("FST_"+i+"_"+j)
+    #make population_pairs and random fst's into a .csv file
     fsts = generate_random_numbers(len(population_pairs))
-    with open("random_fst_test.txt", "w") as rft:
-        rft.write("geneID: " + str(population_pairs).strip("[]"))
-        rft.write("\ngene_1: " + str(fsts).strip("[]"))
-        
-    #write geneID + population_pairs to fst_file
-    #write gene1 + random fst values to fst_file 
+    with open("random_fst_test.csv", "w") as rft:
+        rft.write("geneID," + str(population_pairs).strip("[]").replace("'", ""))
+        rft.write("\ngene_1," + str(fsts).strip("[]"))
 
-make_fst_file(population_pairs)
-# with open("random_fst_test.txt", "w") as rft:
-#     rft.write("geneID: ")
+# make_fst_file(length_of_populations_list)
+make_fst_file(8)
