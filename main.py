065feb477b5df732314fbef7d2da510f05480b4c
Randy Klabacka <r_klabacka@Randys-MacBook-Pro.local>
2023-04-11 16:00:21 -0600

    uncommented empirical data line

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import random_fst_test as rft
 
 rft.make_fst_file(8, 547)
 print('Hello!')
-cfa.identify_significant_loci("random_fst_test.csv", "CategoryAssignments_test.csv")
-#cfa.identify_significant_loci("exons.popgen.csv", "CorrectEcotypeAssignments.csv")
+#cfa.identify_significant_loci("random_fst_test.csv", "CategoryAssignments_test.csv")
+cfa.identify_significant_loci("exons.popgen.csv", "CorrectEcotypeAssignments.csv")
 
