659ae37da11d58f8268f25d32c2bf19d2f801e82
Randy Klabacka <r_klabacka@Randys-MacBook-Pro.local>
2023-04-11 16:18:29 -0600

    updated based on new all_output.csv file

--- create_density_plot.r
+++ create_density_plot.r
@@ -1,13 +1,13 @@
 setwd(insert-path-to-repository)
 # path MUST be in quotes
-dat <- read.csv('rand_example.csv')
+dat <- read.csv('all_output.csv')
 # file MUST be in quotes
 # imports data as dataframe with heading
 
 # look at all geneIDs
-dat$geneID
+dat$GeneID
 # look at all p-values
-dat$p
+dat$P.value
 
 # using ggplot2, create density chart
 # if you don't have ggplot2 package installed, you'll  need to do so as follows:
@@ -15,7 +15,7 @@ install.packages('ggplot2')
 # if you already have it installed, just load it
 library(ggplot2)
 # plot your data! Assign the plot to a variable called 'density_plot'
-density_plot = ggplot(dat, aes(x=p)) + geom_histogram(color="black", fill="white")
+density_plot = ggplot(dat, aes(x=P.value)) + geom_histogram(color="black", fill="white", bins=50)
 # check out your plot and save it write your plot to a file:
 pdf("density_plot.pdf")
 density_plot
