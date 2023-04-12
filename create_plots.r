89b67386a6ec319c18c0b360023f2e4c3dda5ee1
Randy Klabacka <r_klabacka@Randys-MacBook-Pro.local>
2023-04-11 18:45:38 -0600

    changed 'create_density_plot.r' to 'create_plots.r'

---
+++ create_plots.r
@@ -0,0 +1,42 @@
+setwd(insert-path-to-repository)
+# path MUST be in quotes
+dat <- read.csv('all_output.csv')
+# file MUST be in quotes
+# imports data as dataframe with heading
+
+# look at all geneIDs
+dat$GeneID
+# look at all p-values
+dat$P.value
+
+# using ggplot2, create density chart
+# if you don't have ggplot2 package installed, you'll  need to do so as follows:
+install.packages('ggplot2')
+# if you already have it installed, just load it
+library(ggplot2)
+# plot your data! Assign the plot to a variable called 'density_plot'
+density_plot = ggplot(dat, aes(x=P.value)) + geom_histogram(color="black", fill="white", bins=50)
+# check out your plot and save it write your plot to a file:
+pdf("density_plot.pdf")
+density_plot
+dev.off()
+
+# For this section you will use a few other R packages
+library(reshape2)
+library(tidyverse)
+
+dat_melted <- reshape2::melt(dat, id.vars = c("GeneID", "P.value", "Significant"), measure.vars=c("TrueDeltaSame", "TrueDeltaDiff"))
+dat_melted$ordered_var <- with(dat_melted, relevel(variable, "TrueDeltaSame"))
+dat_melted <- dat_melted[order(dat_melted$Significant),]
+
+fst_exons_Plot <- dat_melted %>%
+  ggplot(aes(x=ordered_var,y=value), axis.line = element_line(colour = 'black', size = 3)) +
+  theme_classic() +
+  geom_boxplot(lwd=1, fatten=2, outlier.shape=NA, aes(colour="gray"), show.legend = F) +
+  geom_line(size=1, aes(group=GeneID, linetype=Significant, colour = Significant, alpha=Significant), position = position_dodge(0.2), show.legend = F) + 
+  scale_linetype_manual(values=c("yes" = "solid", "no" = "dotted")) + 
+  scale_color_manual(values=c("no" = "#4B4B4B", "yes" = "black")) + 
+  geom_point(aes(fill=variable,group=GeneID,colour=factor(Significant), alpha=Significant), position = position_dodge(0.2), size = 3, show.legend = F) + 
+  scale_x_discrete(labels=c("Within", "Between")) + 
+  xlab('Comparison Type') + ylab('fst') + # geom_text(aes(label=GeneID), size = 2) + 
+  geom_hline(yintercept=0.02, linetype="dashed", size = 1, color = "#F68A8A")
