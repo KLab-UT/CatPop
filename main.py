e529e4bbc86431810b6c47e41b46e06502f566aa
bayleaflet <bayleejchristensen@gmail.com>
2023-09-28 19:18:31 -0600

    Added error message if user doesn't have proper format for input files

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import calculate_FST_avg as cfa
 import random_fst_test as rft
 import sys
 import argparse
+import os
+
 
 parser = argparse.ArgumentParser(description='Run CatPop on selected files')
 parser.add_argument('--input_prefix', "-i", help='The input file with the previously calculated fsts, correctly formatted.')
@@ -11,9 +13,15 @@ args = parser.parse_args()
 fst_in = args.input_prefix + "_fst.csv"
 cat_in = args.input_prefix + "_categories.csv"
 
+if not (os.path.exists(fst_in) and os.path.exists(cat_in)):
+    print(f"Error: Input files '{fst_in}' and '{cat_in}' do not exist. Please
+            ensure the files you are attempting to use have a 'categories_csv'
+            and a '_fst.csv' prefix")
+    sys.exit(1)
+
 # Allows users to type in names of files in command line
 if len(sys.argv) < 2:
-    print("Usage: python3 main.py <input_prefix>") 
+    print("Usage: python3 main.py <input_prefix>")
     sys.exit(1)
 
 
