c57cadb5f62f0dc9cd5da35df2a1bfa5ac7d4d2d
bayleaflet <bayleejchristensen@gmail.com>
2023-10-01 08:24:49 -0600

    Changes to generate prefix when results are outputted in main.py

--- main.py
+++ main.py
@@ -10,20 +10,24 @@ parser = argparse.ArgumentParser(description='Run CatPop on a category assignmen
 parser.add_argument('-i', "--input_prefix", help='The prefix of the files you wish to use.')
 args = parser.parse_args()
 
+# Finds correct files for input into the program, assigns to variable
 fst_in = args.input_prefix + "_fst.csv"
 cat_in = args.input_prefix + "_categories.csv"
 
+# If both do not exist, Error raised
 if not (os.path.exists(fst_in) and os.path.exists(cat_in)):
     print(f"Error: Input files '{fst_in}' and '{cat_in}' do not exist")
     sys.exit(1)
 
-# Allows users to type in names of files in command line
+# If input prefix is forgotten, error raised.
 if len(sys.argv) < 2:
     print("Usage: python3 main.py <input_prefix>")
     sys.exit(1)
 
+# To create output files with the input prefix
+input_prefix = args.input_prefix
 
 #rft.make_fst_file(8, 547)
-print('Hello! Your results are being calculated')
-cfa.identify_significant_loci(fst_in, cat_in)
+print('Hello! Your results are being calculated.')
+cfa.identify_significant_loci(input_prefix, fst_in, cat_in)
 
