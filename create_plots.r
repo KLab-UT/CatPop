cd046aba30958b3745a9e6e03a4a747a53f289ac
bayleaflet <bayleejchristensen@gmail.com>
2023-10-25 12:38:33 -0700

    Added argument parser to R Script, create_plots.r

--- create_plots.r
+++ create_plots.r
@@ -1,6 +1,20 @@
-setwd("/Users/bayleechristensen/OneDrive - Utah Tech University/FallClasses2023/Research/CatPop")
+# Load the optparse package
+library(optparse)
+# Define the command-line args
+option_list = list(
+  make_option(c("-i", "--input"), type = "character", help = "Input CSV file, with <input_prefix>_all_outputs.csv"),
+  make_option(c("-d", "--directory"), type = "character", help "Set working directory where CatPop located"),
+  make_option(c("-o", "--output"), type = "character", help = "Output PDF file"),
+  make_option(c("-p", "--pvalue"), type = "numeric", default = 0.05, help = "P-value threshold for vertical line"),
+  make_option(c("-b", "--bins"), type = "integer", default = 50, help = "Number of bins for histogram")
+)
+# Parse command line arguments
+opt_parser = OptionParser(usage = "R create_plots.R [options]", option_list = option_list)
+opt = parse_args(opt_parser)
+
+setwd(opt$directory)
 # path MUST be in quotes
-dat <- read.csv('Hu_2021_orchids_all_output.csv')
+dat <- read.csv(opt$input)
 # file MUST be in quotes
 # imports data as dataframe with heading
 
@@ -16,9 +30,9 @@ dat$P.value
 library(ggplot2)
 
 # plot your data! Assign the plot to a variable called 'density_plot'
-density_plot = ggplot(dat, aes(x=P.value)) + geom_vline(xintercept=0.05, linetype="dashed", size = 1, color = "#F68A8A") + geom_histogram(color="black" , fill="white", bins=50)
+density_plot = ggplot(dat, aes(x=P.value)) + geom_vline(xintercept= opt$pvalue, linetype="dashed", size = 1, color = "#F68A8A") + geom_histogram(color="black" , fill="white", bins opt$bins)
 # check out your plot and save it write your plot to a file:
-pdf("density_plot.pdf")
+pdf("opt$output")
 density_plot
 dev.off()
 
@@ -36,7 +50,7 @@ fst_exons_Plot <- dat_melted %>%
   geom_boxplot(lwd=1, fatten=2, outlier.shape=NA, aes(colour="black"), show.legend = F, alpha=0) +
   geom_line(size=1, aes(group=GeneID, linetype=Significant, colour = Significant, alpha=Significant), position = position_dodge(0.2), show.legend = F) +
   scale_linetype_manual(values=c("no" = "dotted","yes" = "solid")) +
-  scale_color_manual(values=c("no" = "#4B4B4B", "yes" = "#e6b002")) + 
+  scale_color_manual(values=c("no" = "#4B4B4B", "yes" = "#e6b002")) +
   geom_point(aes(fill=variable,group=GeneID,colour=factor(Significant), alpha=Significant), position = position_dodge(0.2), size = 3, show.legend = F) +
   scale_x_discrete(labels=c("Within", "Between")) +
   xlab('Comparison Type') + ylab('fst') + # geom_text(aes(label=GeneID), size = 2) +
